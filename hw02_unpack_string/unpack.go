b33029558f4ff66f7110807520ab9728b2aa2866
Maksim Chernyavsky <maksim.chernyavsky@myoffice.team>
2024-06-19 18:53:41 +0300

    linter fix

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -14,15 +14,15 @@ func Unpack(s string) (string, error) {
 	var sb strings.Builder
 	for i := 0; i < len(sRunes); {
 		switch {
-			case unicode.IsDigit(sRunes[i]):
-				return "", ErrInvalidString
-			case i == len(sRunes)-1 || !unicode.IsDigit(sRunes[i+1]):
-				sb.WriteRune(sRunes[i])
-			case unicode.IsDigit(sRunes[i+1]):
-				c, _ := strconv.Atoi(string(sRunes[i+1]))
-				t := string(sRunes[i])
-				sb.WriteString(strings.Repeat(t, c))
-				i++
+		case unicode.IsDigit(sRunes[i]):
+			return "", ErrInvalidString
+		case i == len(sRunes)-1 || !unicode.IsDigit(sRunes[i+1]):
+			sb.WriteRune(sRunes[i])
+		case unicode.IsDigit(sRunes[i+1]):
+			c, _ := strconv.Atoi(string(sRunes[i+1]))
+			t := string(sRunes[i])
+			sb.WriteString(strings.Repeat(t, c))
+			i++
 		}
 		i++
 	}
