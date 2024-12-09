10772e7b529e7c755ba2c2ed0678b909a8720c76
Maksim Chernyavsky <maksim.chernyavsky@myoffice.team>
2024-12-09 18:01:30 +0300

    Fmt

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,14 +1,14 @@
 package hw03frequencyanalysis
 
 import (
-    "container/heap"
-		"strings"
+	"container/heap"
+	"strings"
 )
 
 type Item struct {
-    value    string
-    priority int
-    index int
+	value    string
+	priority int
+	index    int
 }
 
 type PriorityQueue []*Item
@@ -16,45 +16,45 @@ type PriorityQueue []*Item
 func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-		if pq[i].priority != pq[j].priority {
-			return pq[i].priority > pq[j].priority
-		} else {
-			return pq[i].value < pq[j].value 
-		}
+	if pq[i].priority != pq[j].priority {
+		return pq[i].priority > pq[j].priority
+	} else {
+		return pq[i].value < pq[j].value
+	}
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
-    pq[i], pq[j] = pq[j], pq[i]
-    pq[i].index = i
-    pq[j].index = j
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
 }
 
 func (pq *PriorityQueue) Push(x any) {
-    n := len(*pq)
-    item := x.(*Item)
-    item.index = n
-    *pq = append(*pq, item)
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
 }
 
 func (pq *PriorityQueue) Pop() any {
-    old := *pq
-    n := len(old)
-    item := old[n-1]
-    old[n-1] = nil
-    item.index = -1
-    *pq = old[0 : n-1]
-    return item
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
 }
 
 func (pq *PriorityQueue) update(item *Item, value string, priority int) {
-    item.value = value
-    item.priority = priority
-    heap.Fix(pq, item.index)
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
 }
 
 // O(N)
 func Top10(s string) []string {
-//func main() {
+	//func main() {
 	items := map[string]int{}
 	// O(N)
 	words := strings.Fields(s)
@@ -71,12 +71,12 @@ func Top10(s string) []string {
 	i := 0
 	// O(N)
 	for value, priority := range items {
-			heapq[i] = &Item{
-					value:    value,
-					priority: priority,
-					index:    i,
-			}
-			i++
+		heapq[i] = &Item{
+			value:    value,
+			priority: priority,
+			index:    i,
+		}
+		i++
 	}
 	// O(N)
 	heap.Init(&heapq)
@@ -84,9 +84,9 @@ func Top10(s string) []string {
 	result := make([]string, 0, j)
 	// 10log(N)
 	for j > 0 {
-			item := heap.Pop(&heapq).(*Item)
-			result = append(result, item.value)
-			j -= 1
+		item := heap.Pop(&heapq).(*Item)
+		result = append(result, item.value)
+		j -= 1
 	}
 	return result
 }
