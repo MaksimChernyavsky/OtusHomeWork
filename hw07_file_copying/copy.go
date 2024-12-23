6b6c93bdeffc7cce04720095bfb70ff7f6b13b91
Maksim Chernyavsky <maksim.chernyavsky@myoffice.team>
2024-12-24 00:07:06 +0600

    HW7

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb"
 )
 
 var (
@@ -10,6 +15,65 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("open from file: %w", err)
+	}
+	defer fromFile.Close()
+	fromFileInfo, err := fromFile.Stat()
+	if err != nil {
+		return fmt.Errorf("from file stat: %w", err)
+	}
+	if fromFileInfo.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fromFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	toFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("create to file: %w", err)
+	}
+	defer toFile.Close()
+
+	if offset != 0 {
+		_, err := io.CopyN(io.Discard, fromFile, offset)
+		if err != nil {
+			return fmt.Errorf("skip offset in from file: %w", err)
+		}
+	}
+
+	var copySize int64
+	if limit == 0 {
+		copySize = fromFileInfo.Size() - offset
+	} else {
+		copySize = min(limit, fromFileInfo.Size()-offset)
+	}
+	count := 100
+	copyChunk := copySize / int64(100)
+	progress := 0
+	bar := pb.StartNew(count)
+
+	var counter int64
+	for counter < copySize {
+		var c int64
+		progress += 1
+		if progress == 100 {
+			c, err = io.CopyN(toFile, fromFile, copySize-counter)
+		} else {
+			c, err = io.CopyN(toFile, fromFile, copyChunk)
+		}
+		if err != nil {
+			if err != io.EOF {
+				return fmt.Errorf("copy from file")
+			}
+			break
+		}
+		counter += c
+		bar.Increment()
+	}
+	bar.FinishPrint("The End!")
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,68 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		fromPath string
+		toPath   string
+		expPath  string
+		offset   int64
+		limit    int64
+		name     string
+		err      error
+	}{
+		{
+			fromPath: "testdata/input.txt",
+			toPath:   "/tmp/success.txt",
+			expPath:  "testdata/out_offset100_limit1000.txt",
+			offset:   100,
+			limit:    1000,
+			name:     "success",
+			err:      nil,
+		},
+		{
+			fromPath: "/dev/urandom",
+			toPath:   "",
+			expPath:  "",
+			offset:   0,
+			limit:    0,
+			name:     "unsupported_file",
+			err:      ErrUnsupportedFile,
+		},
+		{
+			fromPath: "testdata/input.txt",
+			toPath:   "",
+			expPath:  "",
+			offset:   1000000000000,
+			limit:    1000,
+			name:     "offset_exceeds_file_size",
+			err:      ErrOffsetExceedsFileSize,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("case %s", tt.name), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			err := Copy(tt.fromPath, tt.toPath, tt.offset, tt.limit)
+			if tt.err == nil {
+				require.Empty(t, err, "error after copy must be empty")
+				res, err := os.ReadFile("/tmp/" + tt.name + ".txt")
+				require.Empty(t, err, "error after read all result must be empty")
+				exp, err := os.ReadFile(tt.expPath)
+				require.Empty(t, err, "error after read all expected must be empty")
+				require.Equal(t, exp, res, "incorrect result after copy")
+			} else {
+				require.ErrorIs(t, err, tt.err, "error after copy must be correct")
+			}
+		})
+	}
 }
